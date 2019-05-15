833694e3475d57ed696096b2ce43003c68bf3ac2
A-Engman <anders.p.engman@gmail.com>
2019-05-15 17:41:19 -0400

    Added Side Nav + Adjusted Contact Function on Side Nav. Added Primary JS styling and adjusted CSS proportions and color scheme

--- Robert_Engman_Website/JS/logic.js
+++ Robert_Engman_Website/JS/logic.js
@@ -1,19 +1,19 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function () {
-    myFunction()
-};
+$(document).ready(function () {
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function () {
+        /* Check the location of each desired element */
+        $(".bodyContent").each(function (i) {
+            console.log(this);
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+                console.log(this);
+                $(this).animate({
+                    opacity: "1"
+                }, 3000);
+            }
+        });
+    });
+});
